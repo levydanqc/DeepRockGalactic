f44d92cbeed656303a4c19f959fcb606181621aa
Khygon <regelnomis@hotmail.com>
2022-05-03 08:18:24 -0400

    Ajout styles visuels page de base

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="content">
+    <header>
+      <v-img
+        :src="require('./assets/drg_logo.png')"
+        class="my-3"
+        contain
+        height="200"
+      />
+    </header>
+    <nav class="navbar navbar-dark bg-secondary border">
+      <router-link style="color: #e0ff2f" class="nav-item nav-link" to="/"
+        >Home</router-link
+      >
+      <router-link
+        style="color: #e0ff2f"
+        class="nav-item nav-link"
+        to="/planetes"
+        >Planètes</router-link
+      >
+      <router-link
+        style="color: #e0ff2f"
+        class="nav-item nav-link"
+        to="/reservations"
+        >Réserver</router-link
+      >
+      <router-link
+        style="color: #e0ff2f"
+        class="nav-item nav-link"
+        to="/contrats"
+        >Contrats</router-link
+      >
+      <router-link
+        style="color: #e0ff2f"
+        class="nav-item nav-link"
+        to="/auth/login"
+        >Se Connecter</router-link
+      >
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
@@ -15,10 +50,12 @@
   color: #2c3e50;
 }
 nav {
-  padding: 30px;
+  width: 75vw;
+  font-size: 1.5vw;
+  margin: auto;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #e0ff2f;
     &.router-link-exact-active {
       color: #42b983;
     }
