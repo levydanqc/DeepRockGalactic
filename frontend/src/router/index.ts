cc2c46cc2c145dfad8c8ce2871579e3ccf65bf9b
SimonLeger <80013117+SimonLeger@users.noreply.github.com>
2022-05-12 14:30:05 -0400

    Login (#13)
    * Ajout page login frontend
    
    * Ajout message d'erreur login
    
    * Modifiaction nom de variables + EN to FR
    
    * Correction route
    
    * Correction route login
    
    * Correction méthode d'implémentation des styles
    
    Co-authored-by: Khygon <regelnomis@hotmail.com>
    Co-authored-by: Dan Levy <58832530+levydanqc@users.noreply.github.com>

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 import SearchView from "../views/SearchView.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -6,6 +9,11 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "search",
     component: SearchView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
     path: "/about",
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h1>LOGIN</h1>
+    <form @submit.prevent="login" class="mx-auto p-3">
+      <div class="bg-danger text-white p-1 mx-4 rounded" v-if="erreurs.length">
+        <span v-for="erreur in erreurs" :key="erreur">{{ erreur }}</span>
+      </div>
+      <input
+        class="text-center p-1 my-3 rounded"
+        v-model="email"
+        placeholder="Courriel"
+      />
+      <div class="w-100"></div>
+      <input
+        class="text-center p-1 rounded"
+        v-model="motdepasse"
+        placeholder="Mot de passe"
+        type="password"
+      />
+      <div class="w-100"></div>
+      <button class="btn btn-success px-5 my-3" type="submit">Login</button>
+      <div class="w-100"></div>
+      <router-link
+        to="/signup"
+        class="text-secondary"
+        style="text-decoration: none"
+        >S'inscrire</router-link
+      >
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      email: "",
+      motdepasse: "",
+      erreurs: [],
+    };
+  },
+  methods: {
+    login() {
+      fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: this.email,
+          motdepasse: this.motdepasse,
+        }),
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          } else {
+            this.erreurs.push("Erreur !");
+          }
+        })
+        .then((data) => {
+          localStorage.setItem("token", data.token);
+          this.$router.push("/");
+        });
+    },
+  },
+};
+</script>
+<style lang="scss">
+@import "../assets/styles/variables.css";
+form {
+  border: solid 1px black;
+  width: 25vw;
+  min-width: 300px;
+}
+input {
+  background: var(--background-color);
+}
+</style>
